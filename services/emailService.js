3fb94582535155bed550ecd4c986fa1ce3139b07
Joshua Uwakwe <87578461+JoshUwaks@users.noreply.github.com>
2026-07-15 15:46:02 +0100

    email configuration

--- services/emailService.js
+++ services/emailService.js
@@ -16,7 +16,7 @@ class EmailService {
 
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: parseInt(process.env.EMAIL_PORT) || 587,
+      port: parseInt(process.env.EMAIL_PORT),
       secure: process.env.EMAIL_SECURE === 'true',
       auth: {
         user: process.env.EMAIL_USER,
